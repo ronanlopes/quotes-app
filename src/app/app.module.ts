78a475a75cc6710c2c4eab643a132b3e7ff5fc95
Ronan Lopes <lopesronanufsj@gmail.com>
2017-03-24 15:47:23 -0300

    Páginas e tabs

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { LibraryPage } from '../pages/library/library';
+import { QuotePage } from '../pages/quote/quote';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { SettingsPage } from '../pages/settings/settings';
+import { TabsPage } from '../pages/tabs/tabs';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    FavoritesPage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
